3f18f3242c276d13b3c458d34b21dce5cdb55139
Hanasolo <hanasolo@hotmail.com>
2026-07-16 19:45:03 +0200

    label and vercel analitics implemented

--- src/bootstrap.jsx
+++ src/bootstrap.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { QueryClientProvider } from '@tanstack/react-query'
+import { Analytics } from '@vercel/analytics/react'
 
 import { router } from './app/router/AppRouter.jsx'
 import { AuthProvider } from './contexts/AuthProvider.jsx'
@@ -22,6 +23,7 @@ createRoot(document.getElementById('root')).render(
       <QueryClientProvider client={queryClient}>
         <AuthProvider>
           <RouterProvider router={router} />
+          <Analytics />
         </AuthProvider>
       </QueryClientProvider>
     </ErrorBoundary>
